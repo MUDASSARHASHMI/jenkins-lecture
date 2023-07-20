716c844621edac8faeb4ed77fa79768b8b8e21ef
Mudassar Hashmi <mhashmi1979@gmail.com>
2023-07-20 11:11:20 +0000

    final test

--- testscript.sh
+++ testscript.sh
@@ -6,3 +6,5 @@ echo "Something New"
 echo "adding another for SCM polling"
 echo "adding another for SCM"
 echo "SCM set up with access token from github"
+echo "finally webhook is created and now look at what happens with changes on any branch"
+
