d126d234dd96fbe52dedb0ecd9c7e9e5e35d2eba
Mudassar Hashmi <MUDASSARHASHMI@users.noreply.github.com>
2023-07-24 04:01:28 +0500

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-echo "this script is from Github"
-NUMBER=${($NUMBERONE + $NUMBERTWO)}
+echo "this script is from github"
+NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
-echo "Something New"
-echo "adding another for SCM polling"
-echo "adding another for SCM"
-echo "SCM set up with access token from github"
-echo "finally webhook is created and now look at what happens with changes on any branch"
-
+echo "This was added"
